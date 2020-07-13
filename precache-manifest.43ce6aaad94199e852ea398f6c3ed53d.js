97e84027f8ee519783ad460b052b8e4d1267917b
Andi <andi@Andis-MacBook-Pro.local>
2020-07-13 13:40:04 +0700

    init commit

---
+++ precache-manifest.43ce6aaad94199e852ea398f6c3ed53d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32a71d4d7d6d574d9693105a8c6de617",
+    "url": "/index.html"
+  },
+  {
+    "revision": "24b9dc44c43c366db244",
+    "url": "/static/css/2.e6157a56.chunk.css"
+  },
+  {
+    "revision": "d7f8d9a8742cf15ddc0c",
+    "url": "/static/css/main.523833f0.chunk.css"
+  },
+  {
+    "revision": "24b9dc44c43c366db244",
+    "url": "/static/js/2.5068b164.chunk.js"
+  },
+  {
+    "revision": "3a0685a4e6f8157163147f00af59c181",
+    "url": "/static/js/2.5068b164.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7f8d9a8742cf15ddc0c",
+    "url": "/static/js/main.1842ddc3.chunk.js"
+  },
+  {
+    "revision": "78ec66d2ceed516e159f",
+    "url": "/static/js/runtime-main.483b57af.js"
+  }
+]);
